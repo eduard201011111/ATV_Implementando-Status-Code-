9e437d2e1e0f53aee5f08302070e39f6dd17649d
MykyBalb <mykaelbalbino06@gmail.com>
2025-04-09 15:17:04 -0300

    dudu2

--- Exerc10-MVC-3-Classes/Curso/src/main/java/com/example/Curso/Controller/CursoController.java
+++ Exerc10-MVC-3-Classes/Curso/src/main/java/com/example/Curso/Controller/CursoController.java
@@ -5,63 +5,66 @@ import com.example.Curso.model.Aluno;
 import com.example.Curso.model.Curso;
 
 import java.util.List;
+import java.util.Optional;
 
 public class CursoController {
 
     CursoDb repository = new CursoDb();
 
-    public List<Curso> getAll(){
+    public List<Curso> getAll() {
         return repository.findAll();
     }
 
-    public List<Curso> getByProfessor(String nomeProfessor){
+    public List<Curso> getByProfessor(String nomeProfessor) {
         return repository.findByProfessor(nomeProfessor);
     }
 
-    public List<Curso> getBySala(int sala){
+    public List<Curso> getBySala(int sala) {
         return repository.findBySala(sala);
     }
 
-    public Curso getById(int id){
-        return repository.getById(id);
+    public Optional<Curso> getById(int id) {
+        return Optional.ofNullable(repository.getById(id));
     }
 
-    public boolean insertBanco(Curso curso){
+    public boolean insertBanco(Curso curso) {
         return repository.insert(curso);
     }
 
-    public Curso insertAluno(int idCurso, Aluno aluno){
-        return repository.insertAluno(idCurso, aluno);
+    public Optional<Curso> insertAluno(int idCurso, Aluno aluno) {
+        Curso curso = repository.getById(idCurso);
+        if (curso == null) {
+            return Optional.empty();
+        }
+        repository.insertAluno(idCurso, aluno);
+        return Optional.of(curso);
     }
 
-    // esta funcao fara o mesmo insert do aluno que a funcao a cima, porém com melhores práticas de programção
-    public String insertAlunoMelhorado(int idCurso, Aluno aluno){
+    public String insertAlunoMelhorado(int idCurso, Aluno aluno) {
         Curso curso = repository.getById(idCurso);
-        if(curso == null){
+        if (curso == null) {
             return "Curso não encontrado, para que aluno possa ser inserido!";
         }
         boolean result = repository.insertAlunoMelhorado(curso, aluno);
-        if(result){
+        if (result) {
             return "Aluno inserido com sucesso";
         }
-        return "Não foi possível inserir alunos";
+        return "Não foi possível inserir aluno";
     }
 
-    public Curso update(int id, Curso curso){
+    public Optional<Curso> update(int id, Curso curso) {
         boolean result = repository.update(id, curso);
-
-        if(result){
-            return curso;
+        if (result) {
+            return Optional.of(curso);
         }
-        return null;
+        return Optional.empty();
     }
 
-    public boolean updateAluno(int idCurso, int idAluno, Aluno aluno){
+    public boolean updateAluno(int idCurso, int idAluno, Aluno aluno) {
         return repository.updateAluno(idCurso, idAluno, aluno);
     }
 
-    public boolean delete(int id){
+    public boolean delete(int id) {
         return repository.delete(id);
     }
-
 }
--- Exerc10-MVC-3-Classes/Curso/src/main/java/com/example/Curso/View/CursoView.java
+++ Exerc10-MVC-3-Classes/Curso/src/main/java/com/example/Curso/View/CursoView.java
@@ -3,9 +3,12 @@ package com.example.Curso.View;
 import com.example.Curso.Controller.CursoController;
 import com.example.Curso.model.Aluno;
 import com.example.Curso.model.Curso;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/curso")
@@ -14,54 +17,85 @@ public class CursoView {
     CursoController cursoController = new CursoController();
 
     @GetMapping
-    public List<Curso> getCurso(
+    public ResponseEntity<List<Curso>> getCurso(
             @RequestParam(required = false) String nomeProfessor,
             @RequestParam(required = false) Integer sala)
     {
+        List<Curso> cursos;
         if (nomeProfessor != null) {
-            return cursoController.getByProfessor(nomeProfessor);
+            cursos = cursoController.getByProfessor(nomeProfessor);
         }
         else if (sala != null) {
-            return cursoController.getBySala(sala);
+            cursos = cursoController.getBySala(sala);
         }
         else {
-            return cursoController.getAll();
+            cursos = cursoController.getAll();
         }
+
+        if (cursos.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(cursos, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public Curso getById(@PathVariable int id){
-        return cursoController.getById(id);
+    public ResponseEntity<Curso> getById(@PathVariable int id) {
+        Optional<Curso> curso = cursoController.getById(id);
+        if (curso.isPresent()) {
+            return new ResponseEntity<>(curso.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping
-    public boolean insert(@RequestBody Curso curso){
-        return cursoController.insertBanco(curso);
+    public ResponseEntity<Boolean> insert(@RequestBody Curso curso) {
+        boolean success = cursoController.insertBanco(curso);
+        if (success) {
+            return new ResponseEntity<>(success, HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(success, HttpStatus.BAD_REQUEST);
     }
 
     @PostMapping("/{idCurso}/aluno")
-    public Curso insertAluno(@RequestBody Aluno aluno, @PathVariable int idCurso){
-        return cursoController.insertAluno(idCurso, aluno);
+    public ResponseEntity<Curso> insertAluno(@RequestBody Aluno aluno, @PathVariable int idCurso) {
+        Optional<Curso> cursoOpt = cursoController.insertAluno(idCurso, aluno);
+        if (cursoOpt.isPresent()) {
+            return new ResponseEntity<>(cursoOpt.get(), HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
-    // esta função fatao mesmo insert que a função a cima, porém com melhores práticas de programação
     @PostMapping("/{idCurso}/alunoMelhorado")
-    public String insertAlunoMelhorado(@RequestBody Aluno aluno, @PathVariable int idCurso){
-        return cursoController.insertAlunoMelhorado(idCurso, aluno);
+    public ResponseEntity<String> insertAlunoMelhorado(@RequestBody Aluno aluno, @PathVariable int idCurso) {
+        String response = cursoController.insertAlunoMelhorado(idCurso, aluno);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public Curso update(@RequestBody Curso curso, @PathVariable int id){
-        return cursoController.update(id, curso);
+    public ResponseEntity<Curso> update(@RequestBody Curso curso, @PathVariable int id) {
+        Optional<Curso> updatedCurso = cursoController.update(id, curso);
+        if (updatedCurso.isPresent()) {
+            return new ResponseEntity<>(updatedCurso.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PutMapping("/{idCurso}/aluno/{idAluno}")
-    public boolean update(@PathVariable int idCurso, @PathVariable int idAluno, @RequestBody Aluno aluno){
-        return cursoController.updateAluno(idCurso, idAluno, aluno);
+    public ResponseEntity<Boolean> update(@PathVariable int idCurso, @PathVariable int idAluno, @RequestBody Aluno aluno) {
+        boolean success = cursoController.updateAluno(idCurso, idAluno, aluno);
+        if (success) {
+            return new ResponseEntity<>(success, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(success, HttpStatus.BAD_REQUEST);
     }
 
     @DeleteMapping("/{id}")
-    public boolean delete(@PathVariable int id){
-        return cursoController.delete(id);
+    public ResponseEntity<Boolean> delete(@PathVariable int id) {
+        boolean success = cursoController.delete(id);
+        if (success) {
+            return new ResponseEntity<>(success, HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(success, HttpStatus.NOT_FOUND);
     }
 }
